60efc0a076d56d8f9e9a87166bff9c154902fb13
Chris Alfano <chris@jarv.us>
2019-01-06 04:39:44 -0500

    chore: update nginx from core-plans

--- nginx/plan.sh
+++ nginx/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=nginx
 pkg_origin=emergence
-pkg_version=1.13.10
+pkg_version=1.15.6
 pkg_description="NGINX web server."
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_license=('bsd')
-pkg_source=https://nginx.org/download/nginx-${pkg_version}.tar.gz
+pkg_license=('BSD-2-Clause')
+pkg_source="https://nginx.org/download/nginx-${pkg_version}.tar.gz"
 pkg_upstream_url=https://nginx.org/
-pkg_shasum=336182104d90be3c40c874f7f06f87dbb357da1dc74ea573ad081a0f29a94885
+pkg_shasum=a3d8c67c2035808c7c0d475fffe263db8c353b11521aa7ade468b780ed826cc6
 pkg_deps=(core/glibc core/libedit core/ncurses core/zlib core/bzip2 core/openssl core/pcre)
 pkg_build_deps=(core/gcc core/make core/coreutils)
 pkg_lib_dirs=(lib)
