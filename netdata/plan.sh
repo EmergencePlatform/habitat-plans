e45286edab5541307128d6c339acc5697a55ad5c
Chris Alfano <chris@jarv.us>
2020-04-08 05:22:33 +0000

    feat(netdata): add plan

---
+++ netdata/plan.sh
@@ -0,0 +1,48 @@
+pkg_name=netdata
+pkg_origin=emergence
+pkg_version=1.17.0
+pkg_maintainer="Chris Alfano <chris@jarv.us>"
+pkg_license=("MIT")
+pkg_build_deps=(
+  jarvus/toml-merge
+)
+pkg_wrapped_ident="jarvus/netdata"
+pkg_deps=(
+  "${pkg_wrapped_ident}"
+)
+
+pkg_exports=(
+  [host]=server.address
+  [port]=server.port
+)
+pkg_exposes=(port)
+pkg_svc_run="netdata -D -c ${pkg_svc_config_path}/netdata.conf"
+
+pkg_binds_optional=(
+  [mysql]="port username password"
+  [nginx]="port status_path"
+  [phpfpm]="port status_path"
+)
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  return 0
+}
+
+do_build_config() {
+  do_default_build_config
+
+  build_line "Merging config from ${pkg_wrapped_ident}"
+  cp -nrv "$(pkg_path_for ${pkg_wrapped_ident})"/{config,hooks} "${pkg_prefix}/"
+  toml-merge \
+    "$(pkg_path_for ${pkg_wrapped_ident})/default.toml" \
+    "${PLAN_CONTEXT}/default.toml" \
+    > "${pkg_prefix}/default.toml"
+}
+
+do_strip() {
+  return 0
+}
