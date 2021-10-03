cf7d3ddb4d9cbd3654f9081eb25c6332c0aa24ad
Chris Alfano <chris@jarv.us>
2021-10-03 06:23:11 +0000

    chore(php7-mssql): bump version to v5.9.0

--- php7-mssql/plan.sh
+++ php7-mssql/plan.sh
@@ -1,13 +1,13 @@
 pkg_name=php7-mssql
 pkg_origin=emergence
-pkg_version=5.6.1
+pkg_version=5.9.0
 pkg_maintainer="Jarvus Innovations <hello@jarv.us>"
 pkg_license=("MIT")
 pkg_upstream_url="https://github.com/microsoft/msphpsql"
 pkg_description="Microsoft Drivers for PHP for SQL Server"
 pkg_source="https://github.com/microsoft/msphpsql/archive/v${pkg_version}.tar.gz"
 pkg_dirname="msphpsql-${pkg_version}"
-pkg_shasum="ff05186ea1d33ecd7b258b3a453896cabaa9e944598e7dddb0d6dbe3c8ef1b48"
+pkg_shasum="fe960c4c32c83cd98c84fe85c5cdcb145ed0679ee36a3ab6f16132249a4c0d61"
 pkg_deps=(
   core/gcc-libs
   core/libtool
